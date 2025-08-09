c440dfc67fc01c45ecb424440ddf5af69828cebb
sparrro <116090214+sparrro@users.noreply.github.com>
2025-08-09 02:06:26 +0200

    api call functions for user routes

--- src/scripts/api/accountCalls.ts
+++ src/scripts/api/accountCalls.ts
@@ -14,6 +14,112 @@ export const callResetPassword = async (token: string, password: string) => {
         const data = await response.json();
         return data;
     } catch (error) {
-        console.log(error);
+        return error;
+    }
+};
+
+export const callSignUp = async (username: string, password: string, email: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/user/signup`, {
+            method: "post",
+            body: JSON.stringify({
+                username,
+                password,
+                email
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const callLogIn = async (password: string, username?: string, email?: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/user/login`, {
+            method: "post",
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const callLogOut = async (id: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/user/logout`, {
+            method: "post",
+            body: JSON.stringify({
+                id
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const callRefreshUserToken = async (token: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/user/refresh/${token}`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const callDeleteAccount = async (id: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/user/delete/${id}`, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const callSendPasswordResetEmail = async (email: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/user/sendEmail`, {
+            method: "post",
+            body: JSON.stringify({
+                email
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
     }
 }
